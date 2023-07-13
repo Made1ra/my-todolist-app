6c3f1621304d125bd503c5c1792693568bcd5a98
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2023-07-13 15:41:00 +0300

    Extract container into a separate component

---
+++ src/components/Container.tsx
@@ -0,0 +1,48 @@
+import { styled } from 'styled-components';
+
+const StyledContainer = styled.div`
+    display: flex;
+    justify-content: center;
+
+    background-color: white;
+
+    width: 30%;
+    height: 42.75em;
+    height: 100%;
+
+    border: 0.075em solid black;
+
+    margin: 1.1em auto;
+
+    font-family: 'Lato';
+    font-weight: 500;
+    font-size: 1.125em;
+
+    @media (max-width: 1200px) {
+        width: 40%;
+    }
+
+    @media (max-width: 992px) {
+        width: 50%;
+    }
+
+    @media (max-width: 768px) {
+        width: 60%;
+    }
+
+    @media (max-width: 576px) {
+        width: 95%;
+    }
+`;
+
+type ContainerProps = {
+    children: React.ReactNode;
+};
+
+function Container({ children }: ContainerProps) {
+    return (
+        <StyledContainer>{children}</StyledContainer>
+    );
+}
+
+export default Container;
--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -3,43 +3,9 @@ import { nanoid } from 'nanoid';
 import styled from 'styled-components';
 import { addTask, editTask, toggleTask, removeTask, selectTasks } from '../store';
 import { useSelector, useDispatch } from 'react-redux';
+import Container from './Container';
 import Header from './Header';
 import Heading from './Heading';
-
-const Container = styled.div`
-    display: flex;
-    justify-content: center;
-
-    background-color: white;
-
-    width: 30%;
-    height: 42.75em;
-    height: 100%;
-
-    border: 0.075em solid black;
-
-    margin: 1.1em auto;
-
-    font-family: 'Lato';
-    font-weight: 500;
-    font-size: 1.125em;
-
-    @media (max-width: 1200px) {
-        width: 40%;
-    }
-
-    @media (max-width: 992px) {
-        width: 50%;
-    }
-
-    @media (max-width: 768px) {
-        width: 60%;
-    }
-
-    @media (max-width: 576px) {
-        width: 95%;
-    }
-`;
 
 const TextInput = styled.input`
     font-size: 1.5rem;
